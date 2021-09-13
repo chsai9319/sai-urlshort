f8f47617a39a81466b205ce0f8da6a83409dcecd
chsai9319 <78952784+chsai9319@users.noreply.github.com>
2021-09-13 12:34:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask,request,redirect,url_for
-from flask import render_template
+from flask import Flask,render_template,request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 import string
 import random
 
-app = Flask(__name__,template_folder='templates')
+app = Flask(__name__,template_folder='../templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///url.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
