a8f68574c3c7708f80db39188b19262d7efa6923
chsai9319 <78952784+chsai9319@users.noreply.github.com>
2021-09-13 12:25:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request, redirect, url_for
+from flask import Flask,request,redirect,url_for
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 import string
