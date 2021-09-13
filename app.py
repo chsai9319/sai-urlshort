0a0f2f621904953c0178eabd7d43b8b3896036de
chsai9319 <78952784+chsai9319@users.noreply.github.com>
2021-09-13 12:24:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def home():
             db.session.commit()
             return redirect(url_for("display_short_url", url=short_url))
     else:
-        return render_template(home.html)
+        return render_template('home.html')
 def shorten_url():
     letters = string.ascii_lowercase + string.ascii_uppercase
     while True:
